76601bfef22eb2c465b56ed21d1964afa309bfe7
bpavankalyan <46530943+bpavankalyan@users.noreply.github.com>
2020-05-30 07:52:45 +0530

    Add files via upload

---
+++ A2c.py
@@ -0,0 +1,25 @@
+class AdvantageActorCriticNetwork(nn.Module):
+    def __init__(self, valueNet, policyNet):
+        super(AdvantageActorCriticNetwork, self).__init__()
+
+        self.valueNet = valueNet #RewardNetwork(data["word_to_idx"]).to(device)
+        self.policyNet = policyNet #PolicyNetwork(data["word_to_idx"]).to(device)
+
+    def forward(self, features, captions):
+        # Get value from value network
+        values = self.valueNet(features, captions)
+        # Get action probabilities from policy network
+        probs = self.policyNet(features.unsqueeze(0), captions)[:, -1:, :]        
+        return values, probs
+
+
+rewardNet = RewardNetwork(data["word_to_idx"]).to(device)
+policyNet = PolicyNetwork(data["word_to_idx"]).to(device)
+valueNet = ValueNetwork(data["word_to_idx"]).to(device)
+
+rewardNet.load_state_dict(torch.load('./models/rewardNetwork.pt', map_location={'cuda:0': 'cpu'}))
+policyNet.load_state_dict(torch.load('./models/policyNetwork.pt', map_location={'cuda:0': 'cpu'}))
+valueNet.load_state_dict(torch.load('./models/valueNetwork.pt', map_location={'cuda:0': 'cpu'}))
+
+a2cNetwork = AdvantageActorCriticNetwork(valueNet, policyNet)
+optimizer = optim.Adam(a2cNetwork.parameters(), lr=0.0001)
